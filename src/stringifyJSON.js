2682e33ce93a809428b392be1298265c3d8b0e65
shwetachari <shwetachari@gmail.com>
2017-08-21 12:14:05 -0700

    Corrected stringify test and wrote the stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // base case is primitives
+  // recursive: arrays and objects
+  if (obj === undefined || typeof obj === 'function') {
+    return;
+  } else if (typeof obj !== 'object' && typeof obj !== 'function') {
+    if (typeof obj === 'string') {
+      return '"' + obj + '"';
+    }
+    return obj.toString();
+  } else {
+    if (obj === null) {
+      return 'null';
+    } else if (Array.isArray(obj)) {
+      if (obj.length === 0) {
+        return '[]';
+      }
+      var stringifiedArray = '[';
+      for (var i = 0; i < obj.length; i++) {
+        stringifiedArray += i === 0 ? stringifyJSON(obj[i]) : ',' + stringifyJSON(obj[i]);
+      }
+      stringifiedArray += ']';
+      return stringifiedArray;
+    } else if (typeof obj === 'object') {
+      var stringifiedObject = '{';
+      if (Object.keys(obj).length === 0) {
+        return stringifiedObject + '}';
+      } else {
+        Object.keys(obj).forEach(function(key, position, collection) {
+          //stringifiedObject += i === 0 ? '' : ',';
+          if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+            stringifiedObject += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+            if (position < collection.length - 1) {
+              stringifiedObject += ',';
+            }
+          }    
+        });
+      }
+      return stringifiedObject + '}';
+    }
+    
+  }
 };
