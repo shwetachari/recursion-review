5301c0945072c89ade278dca88f9357f517ddb92
shwetachari <shwetachari@gmail.com>
2017-08-21 12:45:31 -0700

    Completed getElementsByClassName function and fixed test.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,23 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var targetElements = [];
+  var findNodes = function(node) {
+    //base case: no children
+    //recursive case: children
+    
+    var classArray = node.classList ? Array.from(node.classList) : [];
+    if (!node.hasChildNodes() && classArray.includes(className)) {
+      targetElements.push(node);
+    } else {
+      if (classArray.includes(className)) {
+        targetElements.push(node);
+      }
+      node.childNodes.forEach(function(child) {
+        findNodes(child);
+      });
+    }
+  };
+  findNodes(document.body);  
+  return targetElements;
 };
